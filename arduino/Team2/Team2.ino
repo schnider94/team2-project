8e1fdab0919abda419bce266899f792a062f1581
Fabian <fabian@picspi.com>
2019-06-04 10:09:01 +0900

    Arduino code with heartrate and skin response + Bluetooth sending

--- arduino/Team2/Team2.ino
+++ arduino/Team2/Team2.ino
@@ -1,91 +1,74 @@
-#include <CurieBLE.h>
+#include <Wire.h>
+#include <ArduinoBLE.h>
 
-BLEPeripheral blePeripheral; // Our board
+const int GSR=A0;
+int sensorValue=0;
+int gsr_average=0;
 
-// Skin Conductance Sensor BLE vars
-BLEService skinConductanceAndHeartRate("1337D"); // Service for skin conductance
-BLECharacteristic SkinCondChar("1A11", BLERead | BLENotify, 2);
+BLEService anxietyService("1337D");
 
-// Variables for skin conductance
-int oldConductance = 0;  // last heart rate reading from analog input
-int prevMs = 0;
-int packetRateMs = 200; // check packs every 200 ms.
-int ledPin = 13;
+// create characteristics and allow remote device to read and get notifications:
+BLEIntCharacteristic gsr("1A11", BLERead | BLENotify);
+BLEIntCharacteristic heartrate("1A12", BLERead | BLENotify);
 
-// Skin Conductance Sensor BLE vars
-BLECharacteristic heartRateChar("1A12", BLERead | BLENotify, 2);
+void setup(){
+  Serial.begin(115200);
+  while (!Serial);
+  
+  Wire.begin();
 
-// Variables for heart rate.
-int prevBeatRate = 0;
+  while (!BLE.begin()) {
+    Serial.println("Waiting for BLE to start");
+    delay(1);
+  }
+  Serial.println("BLE start");
 
-void updateSkinConductance();
-void updateHeartRate();
+  // set the local name that the peripheral advertises:
+  BLE.setLocalName("AnxioMeter");
+  // set the UUID for the service:
+  BLE.setAdvertisedService(anxietyService);
 
-void setup()
-{
-	Serial.begin(9600);
-	pinMode(ledPin, OUTPUT);   // Pin 13 LED
-	blePeripheral.setLocalName("SkinConductanceAndHeartRate"); // Local name in advertising packets.
-	blePeripheral.setAdvertisedServiceUuid(skinConductanceAndHeartRate.uuid());  // Service UUID
-	blePeripheral.addAttribute(skinConductanceAndHeartRate);
-	// Skin Conductance sensor setup
-	blePeripheral.addAttribute(SkinCondChar);
-	// Skin Conductance sensor setup
-	blePeripheral.addAttribute(heartRateChar);
-	// Activate device, will send advertising packs continuously.
-	blePeripheral.begin();
-	Serial.println("Waiting for connections..");
+  // add the characteristics to the service
+  anxietyService.addCharacteristic(gsr);
+  anxietyService.addCharacteristic(heartrate);
+
+  BLE.addService(anxietyService);
+
+  // start advertising the service:
+  BLE.advertise();
 }
 
-void loop()
-{
-	BLECentral central = blePeripheral.central();
+void loop(){
+  BLEDevice central = BLE.central();
 
-	if(central)
-	{
-		Serial.println("Central connected: " + Serial.println(central.address()));
-		digitalWrite(ledPin, HIGH); // Let's show connection using the LED.
-
-		while(central.connected())
-		{
-			long currentMs = millis();
-			if(currentMs - prevMs >= packetRateMs)
-			{
-				prevMs = currentMs;
-				updateSkinConductance();
-				updateHeartRate();
-			}
-		}
-		// when the central disconnects, turn off the LED:
-		digitalWrite(ledPin, LOW);
-		Serial.println("Central disconnected: " + central.address());
-	}
+  if (central) {
+    updateGSR();
+    updateHeartrate();
+  } 
+  delay(500);
 }
 
-void updateSkinConductance()
-{
-	int conductanceMeasurement = analogRead(A0); // Let's say skin conductance sensor is on A0.
-	int conductance = map(conductanceMeasurement, 0, 1023, 0, 1000); // Let's say we measure between 0 to 1000 mV.
-	if(conductance != oldConductance)
-	{
-		Serial.println("Skin conductance: " + conductance);
-		// Update the characteristic.
-		const unsigned char SkinCondCharA[2] = {0, (char)conductance};
-		SkinCondChar.setValue(SkinCondCharA, 2);
-		oldConductance = conductance;
-	}
+void updateGSR() {
+  long sum=0;
+  for(int i=0;i<10;i++)           //Average the 10 measurements to remove the glitch
+  {
+    sensorValue = analogRead(GSR);
+    sum += sensorValue;
+    delay(5);
+  }
+  gsr_average = sum/10;
+
+  Serial.print("SR: "); // print it
+  Serial.println(gsr_average);
+  gsr.writeValue(gsr_average); 
 }
 
-void updateHeartRate()
-{
-	int heartRateMeasurement = analogRead(A1); // Let's say heart rate sensor is on A1.
-	int beatRate = map(heartRateMeasurement, 0, 1023, 0, 200); // Make sure to update!! these values correctly when obtaining actual sensor!_!!__!_!
-	if(beatRate != prevBeatRate)
-	{
-		Serial.println("Heart Rate: " + beatRate);
-		// Update the characteristic.
-		const unsigned char heartCondCharA[2] = {0, (char)beatRate};
-		SkinCondChar.setValue(heartCondCharA, 2);
-		prevBeatRate = beatRate;
-	}
+void updateHeartrate() {
+  Wire.requestFrom(0xA0 >> 1, 1);    // request 1 bytes from slave device
+  while(Wire.available()) {          // slave may send less than requested
+     int rate = (int) Wire.read();   // receive heart rate value (a byte)
+     Serial.print("HR: "); // print it
+     Serial.println(rate);
+     heartrate.writeValue(rate); 
+  }
 }
